7980767aea1fd762fd36f224e9375f6b9ed6513c
nop90 <nop90nop90@gmail.com>
2014-01-20 15:13:24 +0100

    Create main.h

---
+++ main.h
@@ -0,0 +1,73 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+static char colors[9][3] = {{128,128,128},{0,127,0},{127,0,0},{0,0,127},{0,127,127},{127,0,127},{127,127,0},{63,63,127},{128,128,128}};
+static char logo[120] = 
+    {1,1,1,2,2,0,0,0,5,5,
+     0,1,3,3,2,4,4,0,5,0,
+     0,1,3,2,2,4,0,4,5,0,
+     0,1,3,3,2,4,0,4,0,5,
+     0,1,3,2,2,4,0,4,5,5,
+     0,0,3,3,0,4,4,0,0,0,
+     0,0,0,0,0,0,0,0,0,0,
+     0,0,6,0,6,7,7,0,0,0,
+     0,0,6,0,6,7,0,7,0,0,
+     0,0,6,6,6,7,7,0,0,0,
+     0,0,6,0,6,7,0,7,0,0,
+     0,0,6,0,6,7,7,0,0,0}; 
+
+static char pieces[7][16] = 
+    {{0,0,0,0,
+      0,0,1,1,
+      0,1,1,0,
+      0,0,0,0},
+
+     {0,0,0,0,
+      2,2,0,0,
+      0,2,2,0,
+      0,0,0,0},
+
+     {0,0,0,0,
+      0,3,3,3,
+      0,0,3,0,
+      0,0,0,0},
+
+     {0,0,0,0,
+      0,0,0,4,
+      0,4,4,4,
+      0,0,0,0},
+
+     {0,0,0,0,
+      5,0,0,0,
+      5,5,5,0,
+      0,0,0,0},
+
+     {0,0,0,0,
+      0,6,6,0,
+      0,6,6,0,
+      0,0,0,0},
+
+     {0,0,0,0,
+      7,7,7,7,
+      0,0,0,0,
+      0,0,0,0}};
+
+
+int main();
+void initscreens();
+void drawlogo();
+void initboard(char* board);
+void drawpiece(char x, char y, char rot, char piece, char state);
+void drawsquare(char x, char y, char color);
+char getlevel();
+char playlevel(char level, int* points);
+void startgame(char level);
+char playpiece(char piece, char level, int* points,char* board);
+char gluepiece(char x, char y,char rot, char piece,char* board);
+int isflying (char piece, char x, char y, char rot, char* board);
+void asknewgame();
+
+void getnickname(char* buf);
+ 
+#endif
+
