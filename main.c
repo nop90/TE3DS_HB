414429ffb1b75d2542252273bc25898dd5b57f32
nop90 <nop90nop90@gmail.com>
2014-01-24 13:54:39 +0100

    Update main.c

--- main.c
+++ main.c
@@ -10,7 +10,12 @@
 
  
 int main(){
-  char buf[34];
+  int i;
+  clearscreen(TOP_SCREEN | BOTTOM_SCREEN);
+  for(i=0; i<400; i=i+3) draw_line(i,0,i,239,0,0,255, TOP_SCREEN);
+  draw_rect(19,19,380,220,255,255,0,TOP_SCREEN);  
+  for(i=0; i<320; i=i+3) draw_line(i,0,i,239,0,0,255, BOTTOM_SCREEN);
+  draw_rect(119,19,300,220,255,255,0,BOTTOM_SCREEN);  
   do {
     initscreens();
     drawlogo();
@@ -23,19 +28,32 @@ int main(){
 void drawlogo(){
   char i,j;
   for (i=0; i<10; i++)
-    for (j=0; j<12; j++) if(logo[i+j*10]>0) drawsquare(i,15-j,logo[i+j*10]);
+    for (j=0; j<12; j++) if(logo[i+j*10]>0) drawsquare(i,14-j,logo[i+j*10]);
 }
 
 void initscreens(){
-  int i;
-  clearscreen(TOP_SCREEN | BOTTOM_SCREEN);
-  for(i=0; i<400; i=i+3) draw_line(i,0,i,239,0,0,255, TOP_SCREEN);
-  draw_rect(18,18,380,220,255,255,0,TOP_SCREEN);  
-  draw_fillrect(19,19,379,219,128,128,128,TOP_SCREEN); 
+  draw_fillrect(20,20,379,219,128,128,128,TOP_SCREEN); 
+  draw_fillrect(120,20,299,219,128,128,128,BOTTOM_SCREEN);  
 }
 
+void drawnextpiece(char piece, char state){
+  char i,j,color;
+      for (i=0; i<4; i++)
+        for (j=0; j<4; j++) if(pieces[piece][i+j*4]>0) {
+          color=pieces[piece][i+j*4]*state;
+          draw_fillrect(((14-j)*20),(i+1)*20,((15-j)*20)-1,((i+2)*20)-1,colors[color][0],colors[color][1],colors[color][2], BOTTOM_SCREEN);
+          if(color){ 
+            draw_line(((14-j)*20),(i+1)*20,((15-j)*20)-1,(i+1)*20,colors[color][0]>>1,colors[color][1]>>1,colors[color][2]>>1, BOTTOM_SCREEN);
+            draw_line(((14-j)*20),(i+1)*20,((14-j)*20),((i+2)*20)-1,colors[color][0]>>1,colors[color][1]>>1,colors[color][2]>>1, BOTTOM_SCREEN);
+            draw_line(((15-j)*20)-1,(i+1)*20,((15-j)*20)-1,((i+2)*20)-1,colors[color][0]<<1,colors[color][1]<<1,colors[color][2]<<1, BOTTOM_SCREEN);
+            draw_line(((14-j)*20),((i+2)*20)-1,((15-j)*20)-1,((i+2)*20)-1,colors[color][0]<<1,colors[color][1]<<1,colors[color][2]<<1, BOTTOM_SCREEN);
+          }
+        }
+}
+
+
 void drawpiece(char x, char y, char rot, char piece, char state){
-  char i,j;
+  char i,j,i1,j1;
 
   switch (rot){
     case 0:
@@ -44,15 +62,27 @@ void drawpiece(char x, char y, char rot, char piece, char state){
     break;
     case 1:
       for (i=0; i<4; i++)
-        for (j=0; j<4; j++) if(pieces[piece][(i+1)*4-j-1]>0) drawsquare(x+i,y-j,pieces[piece][(i+1)*4-j-1]*(state&1));
+        for (j=0; j<4; j++){
+          i1=j;
+          j1=3-i;
+          if(pieces[piece][i1 + j1 *4]>0) drawsquare(x+i,y-j,pieces[piece][i1 + j1 *4]*(state&1));
+        } 
     break;
     case 2:
       for (i=0; i<4; i++)
-        for (j=0; j<4; j++) if(pieces[piece][15 - i -(j*4)]>0) drawsquare(x+i,y-j,pieces[piece][15 - i -(j*4)]*(state&1));
+        for (j=0; j<4; j++){
+          i1=3-i;
+          j1=3-j;
+          if(pieces[piece][i1 + j1 *4]>0) drawsquare(x+i,y-j,pieces[piece][i1 + j1 *4]*(state&1));
+        } 
     break;
     case 3:
       for (i=0; i<4; i++)
-        for (j=0; j<4; j++) if(pieces[piece][(3-i)+j]>0) drawsquare(x+i,y-j,pieces[piece][(3-i)+j]*(state&1));
+        for (j=0; j<4; j++){
+          i1=3-j;
+          j1=i;
+          if(pieces[piece][i1 + j1 *4]>0) drawsquare(x+i,y-j,pieces[piece][i1 + j1 *4]*(state&1));
+        } 
     break;
   } 
 }
@@ -74,12 +104,12 @@ char getlevel(){
   char click;
   HID_old=0;
   click=0;
-  draw_string("Use arrows to chose a level then Press start",0,0,255,255,255, BOTTOM_SCREEN);
-  HID_new = read_word(HID);
+  draw_string("Chose a level then Press Select",0,0,255,255,255, BOTTOM_SCREEN);
   while (click==0){
+    HID_new = read_word(HID);
     if(HID_new^HID_old){
       HID_old=HID_new;
-      if (!(HID_new & BUTTON_START)) click=1; 
+      if (!(HID_new & BUTTON_SELECT)) click=1; 
     }
   }
   return 1;
@@ -91,8 +121,8 @@ void asknewgame(){
   HID_old=0;
   click=0;
   draw_string("Press start for a new play",0,24,255,255,255, BOTTOM_SCREEN);
-  HID_new = read_word(HID);
   while (click==0){
+    HID_new = read_word(HID);
     if(HID_new^HID_old){
       HID_old=HID_new;
       if (!(HID_new & BUTTON_START)) click=1; 
@@ -107,129 +137,203 @@ void startgame(char level){
 }
 
 char playlevel(char level, int* points){
-  char board[180];
-  char nextpiece;
+  char buf[13];
+  char linecompleted,i,j,linefull;
+  char board[190];
+  unsigned char nextpiece;
   initscreens();
+  itoa(level,buf);
+  draw_string_rot("LEVEL : ",100,24,255,0,0, BOTTOM_SCREEN);
+  draw_string_rot(buf,164,24,255,255,255, BOTTOM_SCREEN);
+  draw_string_rot("Lines : 25",100,40,0,255,0, BOTTOM_SCREEN);
+  draw_string_rot("Points: ",100,56,0,0,255, BOTTOM_SCREEN);
+  itoa(points[0],buf);
+  draw_string_rot(buf,164,56,255,255,255, BOTTOM_SCREEN);
   initboard(board);
-  nextpiece = (read_word(TIMER) % 7) +1;  
+  nextpiece = read_word(TIMER) % 7;  
   do {  
-    nextpiece = playpiece(nextpiece,level,points,board);
-  } while (nextpiece < 8);
-  return (nextpiece > 8 ? 0 : 1);
+    nextpiece = playpiece(nextpiece,linecompleted,level,points,board);
+  } while ((nextpiece < 8)&(linecompleted<25));
+  if (linecompleted < 25){
+    return 0;
+  } else {
+    for (j=17;j>0;j--) {
+      linefull=1;
+      for (i= 0; i<10;i++) if (board[i+j*10]==0) linefull=0;
+      if (linefull) {
+        // white line
+        for (i= 0; i<10;i++) drawsquare(i, j, 8);
+        // pause 1sec
+        for (i= 0; i<100;i++) draw_fillrect(0,0,320,7,0,0,0,BOTTOM_SCREEN);
+        points[0]+= (100+level*10);
+      }
+    }
+    // pause 2sec
+    for (i= 0; i<200;i++) draw_fillrect(0,0,320,7,0,0,0,BOTTOM_SCREEN);
+    return 1;
+  }
 }
 
 void initboard(char* board){
   int i,j;
   for (i=0;i<10;i++)
-    for (j=0;j<18;j++) board[i+j*10]=0;
+    for (j=0;j<19;j++) board[i+j*10]=0;
 }
 
-char playpiece(char piece, char level, int* points,char* board){
+char playpiece(char piece, char linecompleted, char level, int* points,char* board){
+  char buf[13];
   int i, lines, HID_new,HID_old;
-  char x,y,x1, rot,rot1;
-  char nextpiece;
-  nextpiece = (read_word(TIMER) % 7) +1;  
-  x=4;
-  y=20;
+  char x,y,x1,y1,rot,rot1,notfalling;
+  char nextpiece,state;
+  drawnextpiece(piece, 0);
+  nextpiece = read_word(TIMER) % 7;  
+  drawnextpiece(nextpiece, 1);
+  notfalling=1;
+  x=3;
+  y=19;
   rot=0;
-  HID_old = 0;
-
-  if (isflying(piece,x,y,rot, board)){
+  HID_old = read_word(HID);
+  state=isflying(piece,x,y,rot, board);
+  if (state){
     drawpiece(x,y,rot,piece,1);
-    while (isflying (piece,x,y,rot, board)){
-      for (i=0;i<6000;i++) {
+    while (state){
+      for (i=0;i<(500-level*5 - linecompleted>>2)*notfalling;i++) {
+        draw_fillrect(0,0,320,7,0,0,0,BOTTOM_SCREEN);
         HID_new = read_word(HID);
         x1=x;
         rot1=rot;
         if(HID_new^HID_old){
           HID_old=HID_new;
-          if (!(HID_new & BUTTON_UP) & (x>0)) x1--; 
-          if (!(HID_new & BUTTON_DOWN) & (x<10)) x1++;
-          if (!(HID_new & BUTTON_LEFT)) ;
-          if (!(HID_new & BUTTON_RIGHT)) rot = (rot+1) & 3;
-          if (((x1!=x)|(rot1!=rot))&isflying(piece,x1,y,rot1, board)){
+          if (!(HID_new & BUTTON_UP)) x1--; 
+          if (!(HID_new & BUTTON_DOWN)) x1++;
+          if (!(HID_new & BUTTON_RIGHT)) rot1 = (rot+1) % 4;
+          if (!(HID_new & BUTTON_LEFT)) {
+            x1=x;
+            rot1=rot;
+            notfalling = 0;
+          }
+          if (((x1!=x)|(rot1!=rot)) & isflying(piece,x1,y,rot1, board)){
             drawpiece(x,y,rot,piece,0);
             x=x1;
+            rot=rot1;
             drawpiece(x,y,rot,piece,1);
-          } else {
-            lines = gluepiece(x,y,rot,piece,board);
-            if (lines) points[0] += (50 * lines) << lines;
           }
         }
       }
-      drawpiece(x,y,rot,piece,0);
-      y=y-1;
-      drawpiece(x,y,rot,piece,1);
+      y1=y-1;
+      if (isflying(piece,x,y1,rot, board)){
+        drawpiece(x,y,rot,piece,0);
+        y=y1;
+        drawpiece(x,y,rot,piece,1);
+      } else {
+        lines = gluepiece(x,y,rot,piece,board);
+        points[0] += (50 * lines) << lines;
+        linecompleted += lines;
+        state=0;
+        draw_fillrect(196,40,299,47,128,128,128,BOTTOM_SCREEN);
+        itoa(25 - linecompleted,buf);
+        draw_string_rot(buf,164,40,255,255,255, BOTTOM_SCREEN);
+        draw_fillrect(164,56,299,63,128,128,128,BOTTOM_SCREEN);
+        itoa(points[0],buf);
+        draw_string_rot(buf,196,56,255,255,255, BOTTOM_SCREEN);
+      }
     }
   } else return 8;
   return nextpiece;
 }
 
 char gluepiece(char x, char y,char rot, char piece,char* board){
-  char i,j,k, lines,linefull;
+  char i,j,i1,j1,k, lines,linefull;
 
   switch (rot){
     case 0:
       for (i=0; i<4; i++)
-        for (j=0; j<4; j++) if(pieces[piece][i+j*4]>0) board[x+i + (y-j)*18]=pieces[piece][i+j*4];
+        for (j=0; j<4; j++){
+          if(pieces[piece][i+j*4]>0) board[x+i + (y-j)*10]=pieces[piece][i+j*4];
+        } 
     break;
     case 1:
       for (i=0; i<4; i++)
-        for (j=0; j<4; j++) if(pieces[piece][(i+1)*4-j-1]>0) board[x+i + (y-j)*18]=pieces[piece][(i+1)*4-j-1];
+        for (j=0; j<4; j++){
+          i1=j;
+          j1=3-i;
+          if(pieces[piece][i1 + j1 *4]>0) board[x+i + (y-j)*10]=pieces[piece][i1 + j1 *4];
+        } 
     break;
     case 2:
       for (i=0; i<4; i++)
-        for (j=0; j<4; j++) if(pieces[piece][15 - i -(j*4)]>0) board[x+i +(y-j)*18]=pieces[piece][15 - i -(j*4)];
+        for (j=0; j<4; j++){
+          i1=3-i;
+          j1=3-j;
+          if(pieces[piece][i1 + j1 *4]>0) board[x+i + (y-j)*10]=pieces[piece][i1 + j1 *4];
+        } 
     break;
     case 3:
       for (i=0; i<4; i++)
-        for (j=0; j<4; j++) if(pieces[piece][(3-i)*4+j]>0) board[x+i +(y-j)*18]=pieces[piece][(3-i)*4+j]; 
+        for (j=0; j<4; j++){
+          i1=3-j;
+          j1=i;
+          if(pieces[piece][i1 + j1 *4]>0) board[x+i + (y-j)*10]=pieces[piece][i1 + j1 *4];
+        } 
     break;
   } 
   lines=0;
-  for (j= 0; j<18;j++){
+  for (j= 0; j<19;j++){
     linefull=1;
-    for (i= 0; i<10;i++) if (board[i+j*18]==0) linefull=0;
+    for (i= 0; i<10;i++) if (board[i+j*10]==0) linefull=0;
     if (linefull) {
       lines++;
-// evidenzio
+// white line
       for (i= 0; i<10;i++) drawsquare(i, j, 8);
-// pausa 1sec
-      for (i= 0; i<1000;i++);
+// pause 0.5sec
+      for (i= 0; i<50;i++) draw_fillrect(0,0,320,7,0,0,0,BOTTOM_SCREEN);
 // traslo in basso
-      for (k= j+1; j<18;j++) {
+      for (k= j+1; k<18;k++) {
         for (i= 0; i<10;i++) {
-          drawsquare(i, j, board[i+k*18]);
-          board[i+(k-1)*18] = board[i+k*18];
+          drawsquare(i, k-1, board[i+k*10]);
+          board[i+(k-1)*10] = board[i+k*10];
         } 
-      } 
-// pausa 1sec   
-      for (i= 0; i<1000;i++);
-     j--;
+      }
+      y--;  
+// pause 0.1 sec  
+      for (i= 0; i<10;i++) draw_fillrect(0,0,320,7,0,0,0,BOTTOM_SCREEN);
     } 
   } 
   return lines;
 }
 
 int isflying (char piece, char x, char y, char rot, char* board) {
-  char i,j, check;
-  check=0;
+
+  char i,j,i1,j1, check;
+  check=1;
   switch (rot){
     case 0:
       for (i=0; i<4; i++)
-        for (j=0; j<4; j++) if((pieces[piece][i+j*4]>0) & (((x+i)<0)|((x+i)>9)|((y+j)<0)|(board[x+i + (y-j)*18] !=0))) check=1;
+        for (j=0; j<4; j++) if( (pieces[piece][i+j*4]>0) & ((y-j)<18) & ( ((x+i)<0) | ((x+i)>9) | ((y-j)<0) | (board[x+i + (y-j)*10] >0) ) ) check=0;
     break;
     case 1:
       for (i=0; i<4; i++)
-        for (j=0; j<4; j++) if((pieces[piece][(i+1)*4-j-1]>0) & (((x+i)<0)|((x+i)>9)|((y+j)<0)|(board[x+i + (y-j)*18] !=0))) check=1;
+        for (j=0; j<4; j++){
+          i1=j;
+          j1=3-i;
+          if((pieces[piece][i1+j1*4]>0) & ((y-j1)<18) & (((x+i1)<0)|((x+i1)>9)|((y-j1)<0)|(board[x+i + (y-j)*10] >0))) check=0;
+      }
     break;
     case 2:
       for (i=0; i<4; i++)
-        for (j=0; j<4; j++) if((pieces[piece][15 - i -(j*4)]>0) & (((x+i)<0)|((x+i)>9)|((y+j)<0)|(board[x+i + (y-j)*18] !=0))) check=1;
+        for (j=0; j<4; j++){ 
+          i1=3-i;
+          j1=3-j;
+          if((pieces[piece][i1+j1*4]>0) & ((y-j1)<18) & (((x+i1)<0)|((x+i1)>9)|((y-j1)<0)|(board[x+i + (y-j)*10] >0))) check=0;
+      }
     break;
     case 3:
       for (i=0; i<4; i++)
-        for (j=0; j<4; j++) if((pieces[piece][(3-i)*4+j]>0) & (((x+i)<0)|((x+i)>9)|((y+j)<0)|(board[x+i + (y-j)*18] !=0))) check=1; 
+        for (j=0; j<4; j++){
+          i1=3-j;
+          j1=i;
+          if((pieces[piece][i1+j1*4]>0) & ((y-j1)<18) & (((x+i1)<0)|((x+i1)>9)|((y-j1)<0)|(board[x+i + (y-j)*10] >0))) check=0;
+      }
     break;
   } 
 
